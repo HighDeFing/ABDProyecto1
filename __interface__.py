5eacda7570de8391f314d219af160d580176bb6a
Heider Delgado <heider2112@gmail.com>
2020-01-25 19:57:27 -0400

    Poner las consultas a ser realizadas

--- __interface__.py
+++ __interface__.py
@@ -1,5 +1,5 @@
 from flask import Flask, escape, request, redirect, render_template
-from __init__ import inicio, other_execute
+import consultas
 import sys
 
 app = Flask(__name__)
@@ -8,9 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home.html', methods=['GET', 'POST'])
 def hello():
-    cur = inicio()
-    other_cur = other_execute()
-    return render_template('home.html', cur=cur, other_cur=other_cur)
+    primera = consultas.primera_consulta()
+    segunda = consultas.segunda_consulta()
+    tercera = consultas.tercera_consulta()
+    cuarta = consultas.cuarta_consulta()
+    quinta = consultas.quinta_consulta()
+    sexta = consultas.sexta_consulta()
+    septima = consultas.septima_consulta()
+    octava = consultas.octava_consulta()
+    novena = consultas.novena_consulta()
+    decima = consultas.decima_consulta()
+    return render_template('home.html', pri=primera, seg=segunda, ter=tercera, cua=cuarta, qui=quinta, sex=sexta, sep=septima, oct=octava, nov=novena, dec=decima)
 
 
 if __name__ == "__main__":
