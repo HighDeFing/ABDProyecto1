b9fd96e762729070abbaa5d2e28ed19e0e0e3a6e
Heider Delgado <heider2112@gmail.com>
2020-01-27 00:29:45 -0400

    sexta consulta

--- __interface__.py
+++ __interface__.py
@@ -13,12 +13,31 @@ def hello():
     tercera = consultas.tercera_consulta()
     cuarta = consultas.cuarta_consulta()
     quinta = consultas.quinta_consulta()
-    sexta = consultas.sexta_consulta()
+    sexta_a = consultas.sexta_consulta_a()
+    sexta_b = consultas.sexta_consulta_b()
+    sexta_c = consultas.sexta_consulta_c()
+    sexta_d = consultas.sexta_consulta_d()
+    sexta_e = consultas.sexta_consulta_e()
+    sexta_f = consultas.sexta_consulta_f()
+    sexta_g = consultas.sexta_consulta_g()
+    sexta_h = consultas.sexta_consulta_h()
+    sexta_i = consultas.sexta_consulta_i()
     septima = consultas.septima_consulta()
     octava = consultas.octava_consulta()
     novena = consultas.novena_consulta()
-    decima = consultas.decima_consulta()
-    return render_template('home.html', pri=primera, seg=segunda, ter=tercera, cua=cuarta, qui=quinta, sex=sexta, sep=septima, oct=octava, nov=novena, dec=decima)
+    decima_a = consultas.decima_consulta_a()
+    decima_b = consultas.decima_consulta_b()
+    decima_c = consultas.decima_consulta_c()
+    decima_d = consultas.decima_consulta_d()
+    decima_e = consultas.decima_consulta_e()
+    decima_f = consultas.decima_consulta_f()
+    decima_g = consultas.decima_consulta_g()
+    decima_h = consultas.decima_consulta_h()
+    decima_i = consultas.decima_consulta_i()
+    return render_template('home.html', pri=primera, seg=segunda, ter=tercera, cua=cuarta, qui=quinta,
+                           sex_a=sexta_a, sex_b=sexta_b, sex_c=sexta_c, sex_d=sexta_d, sex_e=sexta_e, sex_f=sexta_f, sex_g=sexta_g, sex_h=sexta_h, sex_i=sexta_i,
+                           sep=septima, oct=octava, nov=novena,
+                           dec_a=decima_a, dec_b=decima_b, dec_c=decima_c, dec_d=decima_d, dec_e=decima_e, dec_f=decima_f, dec_g=decima_g, dec_h=decima_h, dec_i=decima_i)
 
 
 if __name__ == "__main__":
--- consultas.py
+++ consultas.py
@@ -80,11 +80,127 @@ def quinta_consulta():
     conn.close()
 
 
-def sexta_consulta():
+def sexta_consulta_a():
     conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
-    cur.execute('select * from USUARIO')
+    consul = """
+    SELECT BATALLA.ID AS "ID de batalla", BATALLA.USUARIO1 AS "Invocador 1", BATALLA.USUARIO2 AS "Invocador 2", BATALLA.GANADOR AS "Ganador", vsize(BATALLA.ID) + vsize(BATALLA.USUARIO1) + vsize(BATALLA.MAZO1) + vsize(BATALLA.USUARIO2) + vsize(BATALLA.MAZO2)+ vsize(BATALLA.GANADOR) + vsize(BATALLA.PUNTOS) AS "Tamaño del registro" 
+    FROM BATALLA 
+    ORDER BY BATALLA.ID
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_b():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT CARTA.ID AS "ID de carta", vsize(CARTA.ID) + vsize(CARTA.TIPO) + VSIZE(CARTA.NOMBRE) + VSIZE(CARTA.DESCRIPCION) + VSIZE(CARTA.MEJORA) + VSIZE(CARTA.REGION) + VSIZE(CARTA.COSTO) + VSIZE(CARTA.ATAQUE) + VSIZE(CARTA.VIDA) + VSIZE(CARTA.RAREZA) + VSIZE(CARTA.EFECTO) + VSIZE(CARTA.PALABRA_CLAVE) AS "Tamaño del registro"
+    FROM CARTA 
+    ORDER BY CARTA.ID
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_c():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT DIVISION.CODIGO AS "Codigo de division", DIVISION.NOMBRE AS "Nombre de division", DIVISION.DESCRIPCION AS "Descripcion de la division", VSIZE(DIVISION.CODIGO) + VSIZE(DIVISION.NOMBRE) + VSIZE(DIVISION.DESCRIPCION) AS "Tamaño del registro" 
+    FROM DIVISION
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_d():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT INVOCADOR.USUARIO AS "Usuario", INVOCADOR.NOMBRE_INVOCADOR AS "Nombre de invocador", VSIZE(INVOCADOR.USUARIO) + VSIZE(INVOCADOR.SERVIDOR) + VSIZE(INVOCADOR.NOMBRE_INVOCADOR) + VSIZE(INVOCADOR.FECHA_CREACION) AS "Tamaño del registro" 
+    FROM INVOCADOR 
+    ORDER BY INVOCADOR.USUARIO, INVOCADOR.NOMBRE_INVOCADOR
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_e():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT MAZO.ID AS "IDs de mazos", vsize(MAZO.ID) + vsize(MAZO.USUARIO) + vsize(MAZO.SERVIDOR) + vsize(MAZO.CARTA) + vsize(MAZO.NOMBRE)+ vsize(MAZO.CANTIDAD) AS "Tamaño del registro" 
+    FROM MAZO 
+    ORDER BY MAZO.ID
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_f():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT REGION.NOMBRE AS "Nombre de la region", VSIZE(REGION.NOMBRE) + VSIZE(REGION.TIPO_DE_GOBIERNO) + VSIZE(REGION.NIVEL_DE_TECNOLOGIA) + VSIZE(REGION.TOLERANCIA_A_LA_MAGIA) + VSIZE(REGION.DESCRIPCION_AMBIENTE) AS "Tamaño del registro" 
+    FROM REGION 
+    ORDER BY REGION.NOMBRE
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_g():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT RIOTER.USUARIO AS "Usuarios", RIOTER.NOMBRE_INVOCADOR AS "Nombre de invocador", VSIZE(RIOTER.USUARIO) + VSIZE(RIOTER.SERVIDOR) + VSIZE(RIOTER.NOMBRE_INVOCADOR) + VSIZE(RIOTER.FECHA_CONTRATO_INICIO) + VSIZE(RIOTER.FECHA_CONTRATO_FIN) + VSIZE(RIOTER.SALARIO) AS "Tamaño del registro" 
+    FROM RIOTER 
+    ORDER BY RIOTER.USUARIO, RIOTER.NOMBRE_INVOCADOR
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_h():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT SERVIDOR.ID AS "ID de servidor", SERVIDOR.NOMBRE AS "Nombre del servidor", SERVIDOR.IP AS "IP del servidor", VSIZE(SERVIDOR.ID) + VSIZE(SERVIDOR.NOMBRE) + VSIZE(SERVIDOR.INAUGURACION) + VSIZE(SERVIDOR.IDIOMAS) + VSIZE(SERVIDOR.UBICACION) + VSIZE(SERVIDOR.IP) AS "Tamaño del registro" 
+    FROM SERVIDOR 
+    ORDER BY SERVIDOR.ID
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def sexta_consulta_i():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT USUARIO.ID AS "ID de usuario", USUARIO.NOMBRE AS "Nombre del usuario", USUARIO.APELLIDO AS "Apellido del usuario", VSIZE(USUARIO.ID) + VSIZE(USUARIO.SERVIDOR) + VSIZE(USUARIO.CORREO) + VSIZE(USUARIO.CONTRASENA) + VSIZE(USUARIO.NOMBRE) + VSIZE(USUARIO.APELLIDO) + VSIZE(USUARIO.TELEFONO) + VSIZE(USUARIO.FECHA_NACIMIENTO) + VSIZE(USUARIO.SEXO) + VSIZE(USUARIO.DIVISION) + VSIZE(USUARIO.PUNTOS) AS "Tamaño del registro" 
+    FROM USUARIO 
+    ORDER BY USUARIO.ID
+    """
+    cur.execute(consul)
     return cur
     cur.close()
     conn.close()
@@ -136,12 +252,132 @@ def novena_consulta():
     conn.close()
 
 
-def decima_consulta():
+def decima_consulta_a():
     conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
-    cur.execute('select * from USUARIO')
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
     return cur
     cur.close()
     conn.close()
+
+def decima_consulta_b():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_c():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_d():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_e():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_f():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_g():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_h():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+def decima_consulta_i():
+    conn = cx_Oracle.connect("TRYNDAMERE", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT table_name AS "Nombre de la tabla", 8192/avg_row_len AS "Factor de bloqueo de la tabla" 
+    FROM user_tables 
+    ORDER BY table_name
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+
+
+
 
