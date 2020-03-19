6c462afbbe9762c3eb9be3130a3a542a62158b88
Moonvillage96 <60185324+Moonvillage96@users.noreply.github.com>
2020-03-18 21:38:03 -0400

    Update ToBackup.sql

--- Scripts/ToBackup.sql
+++ Scripts/ToBackup.sql
@@ -10,4 +10,21 @@ ALTER DATABASE OPEN;
 -- Crear copia completa de la BD, abriendo una ventada CMD y conectandose a la BD usando RMAN
 RMAN
 
+CONNECT TARGET /
+
 BACKBACKUP DATABASE PLUS ARCHIVELOG;
+
+-- Restaurar y recuperar la BD, abriendo una ventana CMD y conectandose a la BD usando RMAN
+RMAN
+
+CONNECT TARGET /
+
+SHUTDOWN IMMEDIATE;
+
+STARTUP MOUNT;
+
+RESTORE DATABASE;
+
+RECOVER DATABASE;
+
+ALTER DATABASE OPEN;
