f6c6e13a1f5f3e51af44225955519eeb8b448e34
Heider Delgado <heider2112@gmail.com>
2020-03-18 22:32:04 -0400

    consultas del insert

---
+++ consultas_insert.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+
+
+def select_usuarios():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+    SELECT *
+    FROM tryndamere.usuario
+    """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
