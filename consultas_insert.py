3bd6fdf780a99408e02d9ae6dfa3af7dce295d3f
Moonvillage96 <60185324+Moonvillage96@users.noreply.github.com>
2020-03-19 01:25:21 -0400

    Update consultas_insert.py

--- consultas_insert.py
+++ consultas_insert.py
@@ -1,103 +1,51 @@
 import cx_Oracle
 
 
-def select_usuarios():
+def select_batallas1():
     conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
     consul = """
-        SELECT id, unique(servidor), correo, contrasena, nombre, apellido, telefono, fecha_nacimiento, sexo, unique(division), puntos
-        FROM tryndamere.usuario
+        SELECT unique(usuario1), unique(servidor1)
+        FROM tryndamere.batalla
         """
     cur.execute(consul)
     return cur
     cur.close()
     conn.close()
 
-def select_servidores():
+def select_batallas2():
     conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
     consul = """
-        SELECT id, nombre, inauguracion, idiomas, ubicacion, ip
-        FROM tryndamere.servidor
+        SELECT unique(usuario2), unique(servidor2)
+        FROM tryndamere.batalla
         """
     cur.execute(consul)
     return cur
     cur.close()
     conn.close()
     
-def select_divisiones():
+def select_batallas3():
     conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
     consul = """
-        SELECT codigo, nombre, descripcion
-        FROM tryndamere.division
+        SELECT unique(mazo1), unique(usuario1), unique(carta1)
+        FROM tryndamere.batalla
         """
     cur.execute(consul)
     return cur
     cur.close()
     conn.close()
     
- def select_rioter():
+def select_batallas2():
     conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
     consul = """
-        SELECT unique(usuario), unique(servidor), nombre_invocador, fecha_contrato_inicio, fecha_contrato_fin, salario
-        FROM tryndamere.rioter
-        """
-    cur.execute(consul)
-    return cur
-    cur.close()
-    conn.close()
-    
-def select_invocadores():
-    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
-    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
-    cur = conn.cursor()
-    consul = """
-        SELECT unique(usuario), unique(servidor), nombre_invocador, fecha_creacion
-        FROM tryndamere.invocador
-        """
-    cur.execute(consul)
-    return cur
-    cur.close()
-    conn.close()
-    
-def select_mazos():
-    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
-    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
-    cur = conn.cursor()
-    consul = """
-        SELECT id, unique(usuario), unique(servidor), unique(carta), nombre, cantidad
-        FROM tryndamere.mazo
-        """
-    cur.execute(consul)
-    return cur
-    cur.close()
-    conn.close()
-    
-def select_regiones():
-    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
-    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
-    cur = conn.cursor()
-    consul = """
-        SELECT nombre, tipo_de_gobierno, nivel_de_tecnologia, tolerancia_a_la_magia, descripcion_ambiente
-        FROM tryndamere.region
-        """
-    cur.execute(consul)
-    return cur
-    cur.close()
-    conn.close()
-    
-def select_batallas():
-    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
-    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
-    cur = conn.cursor()
-    consul = """
-        SELECT id,  unique(usuario1), unique(servidor1), unique(mazo1), unique(carta1), unique(usuario2), unique(servidor2), unique(mazo2), unique(carta2), ganador, puntos
+        SELECT unique(mazo2), unique(usuario2), unique(carta2)
         FROM tryndamere.batalla
         """
     cur.execute(consul)
@@ -110,8 +58,86 @@ def select_cartas():
     # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
     cur = conn.cursor()
     consul = """
-        SELECT id, tipo, nombre, descripcion, mejora, unique(region), costo, ataque, vida, rareza, efecto, palabra_clave
-        FROM tryndamere.cartas
+        SELECT unique(region)
+        FROM tryndamere.carta
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_mazos1():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(carta)
+        FROM tryndamere.mazos
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_mazos2():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(usuario), unique(servidor)
+        FROM tryndamere.mazo
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_usuarios1():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(division)
+        FROM tryndamere.usuario
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_usuarios2():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(servidor)
+        FROM tryndamere.usuario
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_rioters():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(usuario), unique(servidor)
+        FROM tryndamere.rioter
+        """
+    cur.execute(consul)
+    return cur
+    cur.close()
+    conn.close()
+    
+def select_invocadores():
+    conn = cx_Oracle.connect("TRYNDAMERE_externo_select", "a1234", "localhost/orcl", encoding="UTF-8")
+    # conn = cx_Oracle.connect('TRYNDAMERE/a1234@localhost/orcl')
+    cur = conn.cursor()
+    consul = """
+        SELECT unique(usuario), unique(servidor)
+        FROM tryndamere.invocador
         """
     cur.execute(consul)
     return cur
