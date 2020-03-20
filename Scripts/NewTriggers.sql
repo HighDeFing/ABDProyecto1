4e4bf685766f6cd81e308f4bf8c12857b3444cc8
Heider Delgado <heider2112@gmail.com>
2020-03-19 23:26:23 -0400

    ES `!=` no `=!`

--- Scripts/NewTriggers.sql
+++ Scripts/NewTriggers.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE TRIGGER ganador_batalla
   FOR EACH ROW 
   
   BEGIN
-    IF (:new.ganador =! :new.usuario1) OR (:new.ganador =! :new.usuario2) THEN
+    IF (:new.ganador != :new.usuario1) OR (:new.ganador != :new.usuario2) THEN
      raise_application_error(-20000, 'Dato erroneo');
     END IF;
   END; 
