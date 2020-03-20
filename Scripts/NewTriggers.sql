e02eaaa49a1c1dc7789b19d1bd7ecd82e2bc68f6
Moonvillage96 <60185324+Moonvillage96@users.noreply.github.com>
2020-03-19 23:01:47 -0400

    Update NewTriggers.sql

--- Scripts/NewTriggers.sql
+++ Scripts/NewTriggers.sql
@@ -9,3 +9,42 @@ CREATE OR REPLACE TRIGGER ganador_batalla
     END IF;
   END; 
 /
+
+CREATE OR REPLACE TRIGGER crea_invocador
+ BEFORE INSERT
+  ON tryndamere.invocador
+  FOR EACH ROW
+  
+  BEGIN
+   IF (:new.fecha_creacion > sysdate) THEN
+    raise_application_error(-20000, 'Dato erroneo');
+   END IF;
+  END;
+/
+
+CREATE OR REPLACE TRIGGER crea_usuario
+ BEFORE INSERT
+  ON tryndamere.usuario
+  FOR EACH ROW
+  
+  BEGIN
+   IF (:new.fecha_nacimiento > sysdate) THEN
+    raise_application_error(-20000, 'Dato erroneo');
+   END IF;
+  END;
+/
+
+CREATE OR REPLACE TRIGGER crea_rioter
+ BEFORE INSERT
+  ON tryndamere.rioter
+  FOR EACH ROW
+  
+  BEGIN
+   IF (:new.fecha_contrato_inicio > :new.fecha_contrato_fin) THEN
+    raise_application_error(-20000, 'Dato erroneo');
+   END IF;
+   IF (:new.fecha_contrato_inicio > sysdate) THEN
+    raise_application_error(-20000, 'Dato erroneo');
+   END IF;
+  END;
+/
