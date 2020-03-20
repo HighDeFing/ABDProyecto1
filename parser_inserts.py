0f8c6c3cce3f8789561c758ba330ce3d82798368
Heider Delgado <heider2112@gmail.com>
2020-03-19 23:10:15 -0400

    justificacion agregada

--- parser_inserts.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, escape, request, redirect, render_template
-import consultas
-import sys
-
-
-def parse_agregar_usuario():
-    other = request.form.get('agregar_usuario_servidor')
-    print(other, file=sys.stdout)
-    return
